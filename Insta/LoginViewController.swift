5a03ebde217eecb135ee22033604d62851048336
lpuentes19 <luey.puentes@gmail.com>
2018-10-12 16:53:25 -0600

    Created the login view and viewController file. Linked up outlets and action

---
+++ Insta/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  Insta
+//
+//  Created by Luis Puentes on 10/12/18.
+//  Copyright © 2018 LuisPuentes. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var createAcctButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
+    
+}
