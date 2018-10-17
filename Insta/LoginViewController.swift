f171c866efb6ec8b55308b3e71db86f942440533
lpuentes19 <luey.puentes@gmail.com>
2018-10-17 14:30:33 -0600

    Created a CAShapeLayer and UIBezierPath to create and draw a border around the buttons. I used CABasicaAnimation when drawing the border.

--- Insta/LoginViewController.swift
+++ Insta/LoginViewController.swift
@@ -26,8 +26,10 @@ class LoginViewController: UIViewController {
         setupUI()
         animateTitleLabel()
         animateEmailPasswordLines()
+        animateButtonBorder()
     }
     
+    // Setting up the UI Elements
     func setupUI() {
         
         emailTextField.backgroundColor = .clear
@@ -43,14 +45,16 @@ class LoginViewController: UIViewController {
         passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
         
         loginButton.layer.cornerRadius = 20
-        loginButton.layer.borderWidth = 2
+        //loginButton.layer.borderWidth = 1.5
         loginButton.layer.borderColor = UIColor.white.cgColor
         
         createAcctButton.layer.cornerRadius = 25
-        createAcctButton.layer.borderWidth = 1.5
+        //createAcctButton.layer.borderWidth = 1.5
         createAcctButton.layer.borderColor = UIColor.white.cgColor
     }
     
+    // Animating the title label by shrinking it
+    // Then animating it back to it's original size
     func animateTitleLabel() {
         
         titleLabel.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
@@ -60,6 +64,8 @@ class LoginViewController: UIViewController {
             }, completion: nil)
     }
     
+    // Animating the email and passwrd line
+    // By having a sublayer line draw itself
     func animateEmailPasswordLines() {
         
         let emailTextLayer = CALayer()
@@ -88,6 +94,39 @@ class LoginViewController: UIViewController {
         lineAnimation.duration = 2.5
         emailTextLayer.add(lineAnimation, forKey: "Email Line Animation")
         passwordTextLayer.add(lineAnimation, forKey: "Password Line Animation")
+    }
+    
+    // Animate the borders for each button
+    // To fully circulate around the button
+    func animateButtonBorder() {
+        let loginBorder = CAShapeLayer()
+        let createAcctBorder = CAShapeLayer()
+        
+        // Creating paths around the buttons bounds.origin
+        let loginPath = UIBezierPath(roundedRect: CGRect(origin: loginButton.bounds.origin, size: loginButton.bounds.size), cornerRadius: 20)
+        let createAcctPath = UIBezierPath(roundedRect: CGRect(origin: createAcctButton.bounds.origin, size: createAcctButton.bounds.size), cornerRadius: 25)
+        
+        // Setting the CAShapeLayer's path to the buttons bounds.origin
+        loginBorder.path = loginPath.cgPath
+        loginBorder.strokeColor = UIColor.white.cgColor
+        loginBorder.fillColor = UIColor.clear.cgColor
+        loginBorder.lineWidth = 1.5
+        loginBorder.lineCap = CAShapeLayerLineCap.round
+        loginButton.layer.addSublayer(loginBorder)
+        
+        createAcctBorder.path = createAcctPath.cgPath
+        createAcctBorder.strokeColor = UIColor.white.cgColor
+        createAcctBorder.fillColor = UIColor.clear.cgColor
+        createAcctBorder.lineWidth = 1.5
+        createAcctBorder.lineCap = CAShapeLayerLineCap.round
+        createAcctButton.layer.addSublayer(createAcctBorder)
+        
+        let lineAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        lineAnimation.fromValue = 0
+        lineAnimation.toValue = 1.5
+        lineAnimation.duration = 3.7
+        loginBorder.add(lineAnimation, forKey: "Login Button Animation")
+        createAcctBorder.add(lineAnimation, forKey: "Password Button Animation")
     }
     
     @IBAction func loginButtonTapped(_ sender: Any) {
