efb2ea88dec1cfcfb2c5d7c4813f2d2f2bdb715d
lpuentes19 <luey.puentes@gmail.com>
2018-11-14 20:38:32 -0700

    Added a UITapGestureRecognizer to the postImageView and also conformed to the cameraVC to UIPickerDelegate

--- Insta/View Controllers/CameraViewController.swift
+++ Insta/View Controllers/CameraViewController.swift
@@ -9,14 +9,36 @@
 import UIKit
 
 class CameraViewController: UIViewController {
-    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var captionTextView: UITextView!
     @IBOutlet weak var postButton: UIButton!
     
+    var selectedImage: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handlePostImage))
+        postImageView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handlePostImage() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController, animated: true, completion: nil)
     }
     
     @IBAction func postButtonTapped(_ sender: Any) {
+    }
+}
+
+extension CameraViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            selectedImage = image
+            postImageView.image = image
+        }
+        dismiss(animated: true, completion: nil)
     }
 }
