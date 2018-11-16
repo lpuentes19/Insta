eef25e022a34c46b0676cfde85f1accb86cff883
lpuentes19 <luey.puentes@gmail.com>
2018-11-15 18:54:22 -0700

    Have fixed the constraints between the image and textView in the CameraVC. Also, have completed the post button action logic. Sends it to the storage and database on Firebase.

--- Insta/View Controllers/CameraViewController.swift
+++ Insta/View Controllers/CameraViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseStorage
+import JGProgressHUD
 
 class CameraViewController: UIViewController {
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var captionTextView: UITextView!
     @IBOutlet weak var postButton: UIButton!
     
+    let progressHUD = JGProgressHUD(style: .dark)
+    let errorHUD = JGProgressHUD(style: .dark)
+    let successHUD = JGProgressHUD(style: .dark)
     var selectedImage: UIImage?
     
     override func viewDidLoad() {
@@ -28,7 +34,61 @@ class CameraViewController: UIViewController {
         present(pickerController, animated: true, completion: nil)
     }
     
+    func sendImageDataToDatabase(postImageURL: String) {
+        let ref = Database.database().reference()
+        let postReference = ref.child("posts")
+        if let postID = postReference.childByAutoId().key {
+            let newPostReference = postReference.child(postID)
+            newPostReference.setValue(["postImageURL": postImageURL]) { (error, ref) in
+                if error != nil {
+                    self.progressHUD.dismiss()
+                    
+                    self.errorHUD.textLabel.text = "\(error)"
+                    self.errorHUD.tintColor = .red
+                    self.errorHUD.indicatorView = JGProgressHUDErrorIndicatorView()
+                    self.errorHUD.show(in: self.view)
+                    self.errorHUD.dismiss(afterDelay: 3.0)
+                    return
+                }
+                self.progressHUD.dismiss()
+                
+                self.successHUD.textLabel.text = "Success"
+                self.successHUD.tintColor = .green
+                self.successHUD.indicatorView = JGProgressHUDSuccessIndicatorView()
+                self.successHUD.show(in: self.view)
+                self.successHUD.dismiss(afterDelay: 3.0)
+            }
+        }
+    }
+    
     @IBAction func postButtonTapped(_ sender: Any) {
+        progressHUD.textLabel.text = "Loading..."
+        progressHUD.show(in: self.view)
+        if let profileImage = self.selectedImage, let imageData = profileImage.jpegData(compressionQuality: 0.1) {
+            let imageID = UUID().uuidString
+            let storageRef = Storage.storage().reference(forURL: FirebaseReferences.rootStorageReference).child("posts").child(imageID)
+            storageRef.putData(imageData, metadata: nil, completion: { (metaData, error) in
+                if error != nil {
+                    self.errorHUD.textLabel.text = "\(error)"
+                    self.errorHUD.tintColor = .red
+                    self.errorHUD.indicatorView = JGProgressHUDErrorIndicatorView()
+                    self.errorHUD.show(in: self.view)
+                    self.errorHUD.dismiss(afterDelay: 3.0)
+                    return
+                }
+                storageRef.downloadURL(completion: { (url, error) in
+                    if let postImageImageURL = url?.absoluteString {
+                        self.sendImageDataToDatabase(postImageURL: postImageImageURL)
+                    }
+                })
+            })
+        } else {
+            errorHUD.textLabel.text = "Must add an image to post"
+            errorHUD.tintColor = .red
+            errorHUD.indicatorView = JGProgressHUDErrorIndicatorView()
+            errorHUD.show(in: self.view)
+            errorHUD.dismiss(afterDelay: 3.0)
+        }
     }
 }
 
