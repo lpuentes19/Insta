07b49af4643cf07094844e46e34a7d7cc1fec2f2
lpuentes19 <luey.puentes@gmail.com>
2018-11-17 13:08:10 -0700

    Added all necessary IBOutlets and IBActions.

---
+++ Insta/Views/HomeTableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  HomeTableViewCell.swift
+//  Insta
+//
+//  Created by Luis Puentes on 11/17/18.
+//  Copyright © 2018 LuisPuentes. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var profileNameLabel: UILabel!
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var commentButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var likeCountLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func likeButtonTapped(_ sender: Any) {
+    }
+    @IBAction func commentButtonTapped(_ sender: Any) {
+    }
+    @IBAction func shareButtonTapped(_ sender: Any) {
+    }
+}
