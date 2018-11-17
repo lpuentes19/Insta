4f27f95e444116b18422d8e76e0efc34902ddb2a
lpuentes19 <luey.puentes@gmail.com>
2018-11-17 11:12:22 -0700

    Added a tableView and cell to the HomeView. Also, included the dataSource and delegate methods and made homeView the  delegate. Started a loadPosts method to read from the Database.

--- Insta/View Controllers/HomeViewController.swift
+++ Insta/View Controllers/HomeViewController.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        loadPosts()
     }
+    
+    // MARK: - TableViewDataSource Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func loadPosts() {
+        Database.database().reference().child("posts").observe(.childAdded) { (snapshot) in
+            print(snapshot.value)
+        }
+    }
+    
     @IBAction func logoutButtonTapped(_ sender: Any) {
         
         do {
