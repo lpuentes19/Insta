2eaec67af3a20678c9d03116ffd31afeb491ed79
lpuentes19 <luey.puentes@gmail.com>
2018-11-28 10:35:18 -0700

    Added a commentTableViewCell class and linked up the outlets.

---
+++ Insta/Views/CommentTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CommentTableViewCell.swift
+//  Insta
+//
+//  Created by Luis Puentes on 11/28/18.
+//  Copyright © 2018 LuisPuentes. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
